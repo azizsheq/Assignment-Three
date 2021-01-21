0b75000e72e2412260c5ce0e613e2cf50b697242
azizsheq <azizsheq@gmail.com>
2021-01-22 07:16:17 +0800

    tesing

---
+++ assignment.js
@@ -0,0 +1,27 @@
+// add negative / unexpected values handeling for false value
+// code indentation
+
+// add your repositori link here . . . (Put it on the top)
+
+// then copy the whole file and submit into the submission box
+
+// a function to return a Kilometer unit to Meter
+function kilometerToMeter(km) {
+    
+}
+
+// a function to do budget calculation a = watch (50.00), b = phone(100.00), c = laptop(500.00)
+// will return total cost only amount no strings 
+function budgetCalculator(a, b, c) {
+
+}
+
+//every night (1-10day) 100.00, (11-20day) 80.00, (21day >) 50.00 
+function hotelCost(day) {
+
+}
+
+// input an array of some names,  then find the biggest string and return string
+function megaFriend() {
+
+}
